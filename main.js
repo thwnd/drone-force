2162eee0dc076ba285facc90233ceae51abbfaf3
thwnd <145893801+thwnd@users.noreply.github.com>
2026-04-09 20:13:59 +0900

    Update and rename main.js to src/main.jsx

--- main.js
+++
@@ -1,7 +0,0 @@
-import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom/client";
-import App from "./App.js";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  React.createElement(App)
-);
